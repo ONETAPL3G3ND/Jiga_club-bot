33000c4957677533df77032802ba4bd671d42c6f
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-07-18 22:10:22 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '-'
+guild_ids = [1186031118554767400]
+Admin_id = [896846570018967602, 722502474715627590]
+categories_applications = 1186044547143303250
+channel_race_info = 1187121326855565323
+channel_race_reg = 1187118387491512411
+channel_admin_panel = 1187152028602990742
+
+race_message = 0
+race_admin_massage = 0
